0b759bfc0a774970d86e858a012472558611dde2
Luis Pérez G <lperezgarciag@gmail.com>
2022-07-25 08:41:37 -0500

    Memoria

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Hola mundo";
+const nuevaString = "Hola, Mundo";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 9;
@@ -94,7 +94,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+
 }
 
 function esImpar(num) {
